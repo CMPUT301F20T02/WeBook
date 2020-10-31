73fbc6fafedcacc11f0036e49e422ddc34a2e66d
PeterSunJY <48887827+PeterSunJY@users.noreply.github.com>
2020-10-31 14:28:46 -0600

    Revert "Siyuan9"

--- app/src/main/java/com/example/webook/BorrowerHomepage.java
+++ app/src/main/java/com/example/webook/BorrowerHomepage.java
@@ -35,14 +35,14 @@ public class BorrowerHomepage extends AppCompatActivity {
         bookList.setAdapter(bookAdapter);
 
         final Intent intent = new Intent(this, BorrowerBookProfile.class);
-        bookList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        bookList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+            public boolean onItemLongClick(final AdapterView<?> parent, View view, final int position, long id) {
                 Book selectBook = dataList.get(position);
                 intent.putExtra("selectBook", selectBook);
                 startActivity(intent);
-            }
-
+                return true;
+                }
         });
     }
 }

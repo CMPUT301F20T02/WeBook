e6fba2e7836985ca7092cb2bb12d82f6b365efb3
Tianying Xia <xiaty1@gmail.com>
2020-11-03 17:43:44 -0700

    Activity EditUserProfile

---
+++ app/src/main/java/com/example/webook/EditUserProfile.java
@@ -0,0 +1,17 @@
+package com.example.webook;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_edit_user_profile);
+        
+    }
+
+}

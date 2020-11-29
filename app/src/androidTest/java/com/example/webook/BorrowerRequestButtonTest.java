9eb40d40c33af88d1bdb7ee6aa12dfd7636c3ae7
Rain981012 <siyuan9@ualberta.ca>
2020-11-29 04:17:32 -0700

    BorrowerRequestButtonTest update

---
+++ app/src/androidTest/java/com/example/webook/BorrowerRequestButtonTest.java
@@ -0,0 +1,152 @@
+package com.example.webook;
+
+import android.widget.EditText;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BorrowerRequestButtonTest {
+    private Solo solo;
+    private static DataBaseTestManager dataBaseTestManager;
+    private static Solo soloCls;
+
+    @ClassRule
+    public static ActivityTestRule<MainActivity> ruleCls =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+
+    @BeforeClass
+    public static void clsSetUp() {
+        soloCls = new Solo(InstrumentationRegistry.getInstrumentation(), ruleCls.getActivity());
+        dataBaseTestManager = new DataBaseTestManager();
+        dataBaseTestManager.deleteUser();
+        soloCls.sleep(1000);
+        dataBaseTestManager.createTestData();
+        soloCls.sleep(5000);
+    }
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+
+    /**
+     * Runs before all tests and creates solo instance.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        solo.enterText((EditText) solo.getView(R.id.username_input),"TestBorrower1");
+        solo.enterText((EditText) solo.getView(R.id.pwd_input),"111");
+        solo.clickOnButton("Log in");
+        solo.sleep(1000);
+    }
+
+    @Test
+    public void correctActivity(){
+        solo.assertCurrentActivity("Wrong Activity", BorrowerHomepage.class);
+        solo.clickOnText("REQUESTS");
+        solo.sleep(1000);
+        solo.assertCurrentActivity("Wrong Activity", BorrowerRequestPageActivity.class);
+    }
+
+    @Test
+    public void topButtonsCheck(){
+        solo.assertCurrentActivity("Wrong Activity", BorrowerHomepage.class);
+        solo.clickOnText("REQUESTS");
+        solo.sleep(1000);
+        solo.assertCurrentActivity("Wrong Activity", BorrowerRequestPageActivity.class);
+        assertTrue(solo.waitForText("TestBook5", 1, 5000, true, true));
+        assertTrue(solo.waitForText("TestBook6", 1, 5000, true, true));
+        solo.clickOnText("Accepted");
+        assertTrue(solo.waitForText("TestBook10", 1, 5000, true, true));
+        assertTrue(solo.waitForText("TestBook11", 1, 5000, true, true));
+        assertTrue(solo.waitForText("TestBook12", 1, 5000, true, true));
+        assertTrue(solo.waitForText("Book owner is TestOwner1", 3, 5000, true, true));
+        solo.clickOnText("Borrowed");
+        assertTrue(solo.waitForText("TestBook7", 1, 5000, true, true));
+        assertTrue(solo.waitForText("TestBook8", 1, 5000, true, true));
+        assertTrue(solo.waitForText("TestBook9", 1, 5000, true, true));
+        assertTrue(solo.waitForText("Book owner is TestOwner1", 3, 5000, true, true));
+    }
+/*
+    @Test
+    public void BorrowerRequestDeliveryCheck() {
+        solo.assertCurrentActivity("Wrong Activity", BorrowerHomepage.class);
+        solo.clickOnText("REQUESTS");
+        solo.sleep(1000);
+        solo.assertCurrentActivity("Wrong Activity", BorrowerRequestPageActivity.class);
+        solo.clickOnText("Accepted");
+        solo.sleep(2000);
+        solo.goBack();
+        solo.goBack();
+        solo.enterText((EditText) solo.getView(R.id.username_input),"TestBorrower1");
+        solo.enterText((EditText) solo.getView(R.id.pwd_input),"111");
+        solo.clickOnButton("Log in");
+        solo.assertCurrentActivity("Wrong Activity", BorrowerHomepage.class);
+        solo.clickOnText("REQUESTS");
+        solo.sleep(1000);
+        solo.assertCurrentActivity("Wrong Activity", BorrowerRequestPageActivity.class);
+        solo.clickOnText("Accepted");
+        solo.sleep(1000);
+        solo.clickOnText("TestBook10");
+        solo.assertCurrentActivity("Wrong Activity", BorrowerRequestDelivery.class);
+        assertTrue(solo.waitForText("Title: TestBook10", 1, 5000, true, true));
+        assertTrue(solo.waitForText("ISBN: 0100000000000", 1, 5000, true, true));
+        assertTrue(solo.waitForText("Owner: TestOwner1", 1, 5000, true, true));
+        assertTrue(solo.waitForText("Status: accepted", 1, 5000, true, true));
+        assertTrue(solo.waitForText("2025-12-5", 1, 5000, true, true));
+        solo.clickOnText("DELIVER SCAN");
+        solo.assertCurrentActivity("Wrong Activity", CodeScanner.class);
+    }
+
+ */
+    @Test
+    public void BorrowerBorrowedDeliveryCheck(){
+        solo.assertCurrentActivity("Wrong Activity", BorrowerHomepage.class);
+        solo.clickOnText("REQUESTS");
+        solo.sleep(1000);
+        solo.assertCurrentActivity("Wrong Activity", BorrowerRequestPageActivity.class);
+        solo.clickOnText("Borrowed");
+        solo.sleep(1000);
+        solo.clickOnMenuItem("TestBook7");
+        solo.sleep(1000);
+        solo.assertCurrentActivity("Wrong Activity", BorrowerReturn.class);
+        assertTrue(solo.waitForText("Title: TestBook7", 1, 5000, true, true));
+        assertTrue(solo.waitForText("ISBN: 7000000000000", 1, 5000, true, true));
+        assertTrue(solo.waitForText("Owner: TestOwner1", 1, 5000, true, true));
+        assertTrue(solo.waitForText("Status: borrowed", 1, 5000, true, true));
+        assertTrue(solo.waitForText("2020-9-22", 1, 5000, true, true));
+        solo.clickOnText("Deliver Scan");
+        solo.assertCurrentActivity("Wrong Activity", CodeScanner.class);
+    }
+
+
+
+    @After
+    public void tearDown() {
+        solo.finishOpenedActivities();
+    }
+
+
+    @AfterClass
+    public static void clsTearDown() {
+        dataBaseTestManager.deleteTestData();
+        soloCls.sleep(5000);
+    }
+}
